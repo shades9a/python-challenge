dff4b55bd47f962a488897ff678139caa89b3d3d
shades9a <81335214+shades9a@users.noreply.github.com>
2021-04-09 15:48:37 -0500

    Add files via upload

---
+++ PyBank/main.py
@@ -0,0 +1,7 @@
+import os
+import csv
+csvpath = os.path.join('Resources','budget_data.csv')
+with open(csvpath, 'r') as file_handler:
+    lines = file_handler.read()
+    print(lines)
+    print(type(lines))
